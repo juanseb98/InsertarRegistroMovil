4b90d716529bea21897d0535de4d8876d975728f
sastian <sastian@DESKTOP-6AMABGO>
2018-11-30 10:59:02 +0100

    Modelo comentado

--- src/insertarDatos/modelo/ConeccionBD.java
+++ src/insertarDatos/modelo/ConeccionBD.java
@@ -1,10 +1,5 @@
 package insertarDatos.modelo;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -16,9 +11,9 @@ import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
- * Modelo para realizar consultas en base de datos SQLite
+ * Clase encargada de relizar acciones sobre la base de datos SQLite
  *
- * @author DAM-2
+ * @author Juan Sebsatián González Sánchez
  */
 public class ConeccionBD {
 //ACORTADOR DE URL https://bitly.com/
@@ -42,22 +37,47 @@ public class ConeccionBD {
         }
     }
 
+    /**
+     * Metodo encargado de realizar consultas que se le pasa por parametro y
+     * devolver el resultado de la consulta.
+     *
+     * @param consulta sentencia de consulta
+     * @return Devuelve el resultSet con el resultado de la consulta.
+     * @throws SQLException
+     */
     public ResultSet realizarConsulta(String consulta) throws SQLException {
         return sentencia.executeQuery(consulta);
     }
 
+    /**
+     * Metodo encargado de Insertar en la base de datos.
+     *
+     * @param insert Sentencia insert.
+     * @throws SQLException
+     */
     public void realizarInsert(String insert) throws SQLException {
         sentencia.executeUpdate(insert);
     }
 
+    /**
+     * Metodo encargado de cerrar la coneccion con la base de datos
+     */
     public void cerrarConeccion() {
         try {
             conexion.close();
         } catch (SQLException ex) {
-            Logger.getLogger(ConeccionBD.class.getName()).log(Level.SEVERE, null, ex);
+            //Logger.getLogger(ConeccionBD.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
+    /**
+     * Metodo que se encarga de crear un preparedStament con la sentencia pasada
+     * por parametro y devolverlo para poder operar con el
+     *
+     * @param prepareStament Sentencia de prepareStament
+     * @return
+     * @throws SQLException
+     */
     public PreparedStatement getPrepareStament(String prepareStament) throws SQLException {
         return conexion.prepareStatement(prepareStament);
     }
